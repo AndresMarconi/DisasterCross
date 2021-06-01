2057252fbe07bff0d9eaca325b79cc3245914417
Andres <andres_sandro@hotmail.com>
2021-06-01 14:09:55 -0300

    add lines

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,17 @@
 <template>
   <v-container class="fill-height">
-    <v-row class="fill-height">
+    <v-row class="fill-height d-flex justify-center">
       <v-col 
         v-for="world in worlds" 
         :key="world.name"
-        class="d-flex fill-height justify-center  level-column py-5">
+        class="level-column">
           <div class="d-flex align-center justify-center levelSelector">
             <router-link :to="makeRoute(world.docId)">
               {{world.name}}
             </router-link>
           </div>
+          <div class="first-line"></div>
+          <div class="second-line"></div>
       </v-col>
     </v-row>
   </v-container>
@@ -53,20 +55,67 @@ export default {
 }
 </script>
 
-<style scoped>
-  .levelSelector{
-    width: 100px;
-    height: 100px;
-    border-radius: 100%;
-    background-color: aquamarine;
-  }
-  .level-column:nth-child(1){
-    margin-top: 5vh;
-  }
-  .level-column:nth-child(2){
-    margin-top: 25vh;
-  }
-  .level-column:nth-child(3){
-    margin-top: 15vh;
-  }
+<style lang="sass" scoped>
+
+  $margin1: 5
+  $margin2: 25
+  $margin3: 15
+
+  .levelSelector
+    width: 100px
+    height: 100px
+    border-radius: 100%
+    background-color: aquamarine
+
+  .level-column
+    margin: 0
+    padding: 0
+    flex-grow: 0
+
+  @for $i from 1 through 3
+    $m1: 0
+    $m2: 0
+    @if $i == 1
+      $m1: $margin1
+      $m2: $margin2
+    @else if $i == 2
+      $m1: $margin2
+      $m2: $margin3
+    @else if $i == 3
+      $m1: $margin3
+      $m2: $margin3
+
+    .level-column:nth-child(#{$i})
+      margin-top: #{$m1}vh
+      display: flex
+      flex-direction: row
+
+    @if ($m1 < $m2)
+      $height: ($m2 - $m1)
+      .level-column:nth-child(#{$i}) .first-line
+        width: 4vw
+        height: #{$height}vh
+        border-right: solid
+        border-top: solid
+        margin-top: 50px
+      .level-column:nth-child(#{$i}) .second-line
+        margin-right: 0
+        width: 4vw
+        border-bottom: solid
+        margin-top: 50px
+        height: #{$height}vh
+    @else if ($m1 > $m2)
+      $height: ($m1 - $m2)
+      .level-column:nth-child(#{$i}) .first-line
+        width: 4vw
+        height: #{$height}vh
+        border-right: solid
+        border-bottom: solid
+      .level-column:nth-child(#{$i}) .second-line
+        margin-right: 0
+        width: 4vw
+        border-top: solid
+  
+  
+  
 </style>
