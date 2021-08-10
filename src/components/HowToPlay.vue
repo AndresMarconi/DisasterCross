ab6a2cc9e4c60d60d0d8e41b25316ba754883792
Andres <andres_sandro@hotmail.com>
2021-08-10 17:14:08 -0300

    fix

--- src/components/HowToPlay.vue
+++ src/components/HowToPlay.vue
@@ -4,12 +4,12 @@
       <v-btn @click="open">¿Como jugar?</v-btn>
     </v-col>
     <v-dialog v-model="dialog">
-      <v-card role="dialog" aria-labelledby="dialog1Title" aria-describedby="dialog1Desc" >
+      <v-card>
         <v-card-title class="d-flex justify-center my-5"> 
-          <h2 id="dialog1Title" class="headline text-center">¿Como jugar DissasterCross?</h2>
+          <h2 class="headline text-center">¿Como jugar DissasterCross?</h2>
         </v-card-title>
         <v-card-text>
-          <p role="alert" id="dialog1Desc" class="body-1 text-center">
+          <p role="alert" class="body-1 text-center">
             Seleccione el mundo y el nivel mas avanzado posible, desafíe su conocimiento sobre catástrofes y cómo reaccionar frente a una.
             <br><br>
             Completa las palabras del acróstico utilizando la descripción que aparece en pantalla.
